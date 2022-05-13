1898dca2f7b1162c9b89b908c4ca6f0ebe5718ea
lucasmenchon <it.lucas@outlook.com>
2022-05-13 00:40:25 -0300

    add projetos, pratica e estudo

---
+++ ExDIO/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExDIO
+{
+    public class Pessoa
+    {
+        public int Idade { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Documento { get; set; } 
+
+        public Pessoa Clone()
+        {
+            return new Pessoa()
+            {
+                Nome = this.Nome,
+                Documento = this.Documento,
+                Idade = this.Idade,
+            };
+        }
+
+    }
+}
---
+++ ExDIO/StructPessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExDIO
+{
+    public struct StructPessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Documento { get; set; }
+    }
+}
